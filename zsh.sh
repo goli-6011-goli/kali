e2336a021160c9d1de52a3965676e842367180bf
Goli <134586683+goli-6011@users.noreply.github.com>
2024-02-05 19:02:57 +0330

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -14,8 +14,14 @@ apt install lolcat -y
 
 apt install zsh -y
 
+cp ./apt.up /usr/bin/apt.up
+
 ############################################################
 
 dpkg-reconfigure kali-grant-root
 
 chsh -s /usr/bin/zsh
+
+echo sudo su >> ../../.bashrc
+
+cp ./.zshrc /root/.zshrc
