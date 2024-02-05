07d67fc93d7b3847813580f30dd1ab814b6c626e
Goli <134586683+goli-6011@users.noreply.github.com>
2024-02-05 18:45:38 +0330

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -5,7 +5,9 @@
 #apt upgrade -y
 
 #apt autoremove -y
+
 ############################
+
 apt install figlet -y
 
 apt install lolcat -y
@@ -13,6 +15,7 @@ apt install lolcat -y
 apt install zsh -y
 
 ############################################################
+
 dpkg-reconfigure kali-grant-root
 
-chsh /usr/bin/zsh
+chsh -s /usr/bin/zsh
