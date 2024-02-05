d2eb5549810560e93923b0ead0e1aa2ea5ee6ea1
Goli <134586683+goli-6011@users.noreply.github.com>
2024-02-05 18:33:59 +0330

    Create zsh.sh

---
+++ zsh.sh
@@ -0,0 +1,18 @@
+apt update
+
+apt list --upgradable
+
+apt upgrade -y
+
+apt autoremove -y
+############################
+apt install figlet -y
+
+apt install lolcat -y
+
+apt install zsh -y
+
+############################################################
+dpkg-reconfigure kali-grant-root
+
+chsh /usr/bin/zsh
