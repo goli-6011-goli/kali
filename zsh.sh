c7edb6f2a9c2f3dfe57852533dc0065b9e47f658
Goli <134586683+goli-6011@users.noreply.github.com>
2024-02-06 11:50:38 +0330

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -25,3 +25,11 @@ sudo chsh -s /usr/bin/zsh
 sudo echo sudo su >> ../../.bashrc
 
 sudo cp ./.zshrc /root/.zshrc
+
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+sudo git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+sudo git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
